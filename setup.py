fcdd3984921d51738d9453c4cb26f7965aa648f8
nick-keller <nicolas.kllr@gmail.com>
2023-04-21 18:12:55 +0200

    Add README

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tggl',
-    version='1.0.0',
+    version='1.1.1',
     description='Tggl python client',
     packages=find_packages(),
     install_requires=[],
